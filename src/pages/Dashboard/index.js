10063ca9c960b1f83724eae90b98c0a93f35f9b8
mbalabanov <marincomics@gmail.com>
2022-10-03 22:03:34 +0300

    Adds total number of people in each column to title

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -4,15 +4,23 @@ import { Link } from "react-router-dom";
 function Dashboard(props) {
   const { hiredPeople, people } = props;
 
+  const numberOfPeople = people.length;
+  const numberOfHiredPeople = hiredPeople.length;
+
   return (
     <main className="dashboard-layout">
       <section>
-        <h2>People</h2>
+        <h2>
+          People <span>({numberOfPeople})</span>
+        </h2>
         <PeopleList people={people} colName="PeopleCol" />
       </section>
       <section>
-        <h2>Hired People</h2>
-        {hiredPeople.length > 0 && (
+        <h2>
+          Hired People
+          {numberOfHiredPeople > 0 && <span> ({numberOfHiredPeople})</span>}
+        </h2>
+        {numberOfHiredPeople > 0 && (
           <Link to="/edit_hired" className="viewButton">
             Edit
           </Link>
